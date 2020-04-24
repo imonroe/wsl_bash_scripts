b438cca4ecfa4c9a68be188b80d3835950663b6f
Ian Monroe <ian@ianmonroe.com>
2020-04-24 11:22:22 -0600

    Update setup_stack.sh
    added php7.3-bz2 extension.

--- setup_stack.sh
+++ setup_stack.sh
@@ -33,7 +33,7 @@ try sudo apt-get install curl
 
 echo 'Installing PHP7.3 and deps.'
 try sudo apt-get install php7.3
-try sudo apt-get install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd  php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json
+try sudo apt-get install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd php7.3-bz2 php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json
 try sudo apt-get install php7.3-intl php-xml php-imagick php-bz2 php-pcov imagemagick php7.3-imagick
 
 echo 'Enabling PHP 7.3 support in Apache2'
